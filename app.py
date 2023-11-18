ff4fb09610fa60804e6e9abead5a218ee7c0d392
Doğukan Ürker <dogukanurker@icloud.com>
2023-11-18 11:10:32 +0300

    initial files

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, redirect
+import socket
+import secrets
+
+app = Flask(__name__)
+app.secret_key = secrets.token_urlsafe(32)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run(
+        debug=True,
+        host=socket.gethostbyname(socket.gethostname()),
+    )

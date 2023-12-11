d1d6848547350aa8030db22b0657a6a7e31e62f8
Doğukan Ürker <dogukanurker@icloud.com>
2023-12-11 19:55:37 +0300

    wrong file fixed

--- app/data/locations.py
+++ app/data/locations.py
@@ -11,9 +11,9 @@ locationImage = [
     "",
     "",
     "",
-    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/adramytteionAncientCity/1651344_620x410.jpg",
+    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/adramytteionAncientCity/adramytteionAncientCity1.jpg",
     "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/hareketliMuseum/images.jfif",
-    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/adramytteionAncientCity/1651344_620x410.jpg",
+    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/pelitkoy/maxresdefault.jpg",
 ]
 
 locationSound = [
@@ -22,5 +22,5 @@ locationSound = [
     "",
     "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/adramytteionAncientCity/adramytteionAncientCity.ogg",
     "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/hareketliMuseum/hareketliMuseum.ogg",
-    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/adramytteionAncientCity/adramytteionAncientCity.ogg",
+    "https://raw.githubusercontent.com/DogukanUrker/BurhaniyeAPP/main/app/db/Locations/pelitkoy/adramytteionAncientCity.ogg",
 ]

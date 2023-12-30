7cf6e5bb9eac177ca8fbe689550fa3d4f79dee8a
Doğukan Ürker <dogukanurker@icloud.com>
2023-12-30 12:16:43 +0300

    added sound and focus effect to questions

--- app/static/js/q9.js
+++ app/static/js/q9.js
@@ -1,8 +1,12 @@
 function trueAnswer() {
+  var audio = document.getElementById("audioCorrect");
+  audio.play();
   alert("🎉 TEBRİKLER DOĞRU CEVAP 🎉");
   window.location.replace("/tebrikler3");
 }
 
 function falseAnswer() {
+  var audio = document.getElementById("audioWrong");
+  audio.play();
   alert("☹️ ÜZGÜNÜM TEKRAR DENEMELİSİN ☹️");
 }
